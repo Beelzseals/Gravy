2fb211a458aa3aefde53f455ab2f60d99225710a
Beelzseals <restyanszkij@gmail.com>
2026-02-17 20:27:44 +0100

    feat: add actions as part of authorization

---
+++ src/core/authorization/actions.ts
@@ -0,0 +1,7 @@
+export type ProjectAction =
+  | "project:create"
+  | "project:view"
+  | "project:update"
+  | "project:delete";
+
+export type OrgAction = "org:update" | "org:delete" | "org:invite";
